21d900f0e95bddeee9505b4f3ce5efe135f7d1cc
nuggex <nuggex@gmail.com>
2020-09-07 13:14:54 +0300

    Uppgift 1

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
+
+
+
 window.onload = function() {
-    // Lyssna efter händelser
-}
-var editTitle = function(e) {
-    // Ändra "team-title"
-};
+    var profile = document.getElementsByClassName('profile');
+    var team = document.getElementsByClassName('team-member');
 
-function effectOn(id){
-    // Rita en ram runt personen, kanske lite drop shadow?
-}
+    console.log(team);
+    
+    for(let i=0; i< team.length; i++)
+    {
+        team[i].addEventListener("mouseover", function( event){
+            console.log(event);
+            team[i].firstElementChild.style.filter = "drop-shadow(0 0 0.75rem red)";
+       })
+       team[i].addEventListener("mouseout", function( event){
+        team[i].firstElementChild.style.filter  = "drop-shadow(0 0 0rem red)";
+       })
+       team[i].addEventListener("click", function(event){
+           var x = window.prompt("nytt namn");
+           team[i].querySelector(".team-title").innerHTML = x;
+       })
+    }
 
-function effectOff(id){
-    // Stäng av effekten när musen inte längre är ovanför personen
+    
 }
